fd2a221683b42090dc77e9db8d4c8646f41ade9d
josecparreira <73337511+josecparreira@users.noreply.github.com>
2020-12-02 09:27:21 +0000

    FAQ

--- resources/views/paginas/frontend/faq.blade.php
+++ resources/views/paginas/frontend/faq.blade.php
@@ -1,17 +1,15 @@
 <!doctype html>
 <html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FAQ</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="faq.css">
+    @section('title', 'FAQ')
+    @include('includes.frontend.head')
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/paginas/frontend/faq.css') }}">
 </head>
 
 <body>
     <div class="banner">
-        <img class="img-fluid" src="faq-banner.jpg" alt="banner">
+        <img class="img-fluid" src="{{ URL::asset('assets/img/paginas/frontend/faq-banner.jpg') }}" alt="banner">
     </div>
     <div class="container-fluid">
         <div class="row">
