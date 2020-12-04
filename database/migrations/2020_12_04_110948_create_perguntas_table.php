85bc6c566add091f4813bbee19c3fb35b782bfb6
josecparreira <73337511+josecparreira@users.noreply.github.com>
2020-12-04 11:16:48 +0000

    tabelas ZÉ

---
+++ database/migrations/2020_12_04_110948_create_perguntas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerguntasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perguntas', function (Blueprint $table) {
+            $table->id();
+            $table->string('pergunta');
+            $table->string('categoria');
+            $table->string('resposta');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perguntas');
+    }
+}
