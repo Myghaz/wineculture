3ef8b4ec92e05b382e2f5bb59c050ed6c155dc61
Meireles09 <guimeireles90@gmail.com>
2020-12-12 12:03:08 +0000

    some changes in blog

--- resources/views/paginas/frontend/previewblog.blade.php
+++ resources/views/paginas/frontend/previewblog.blade.php
@@ -9,28 +9,20 @@
 </head>
 
 <body>
-    <div class="class1">
-    @foreach($previewblog as $value)
-    <div class="class2">
-    <img src="{{ url('assets/img/paginas/frontend/blog/' .$value->img)}}" width="1300px" height="300px" ></img>
-    </div>
-    <br>
-    <header>
-        <h1>{{$value->titulo}}</h1>
-        <p>Publicado por <em>{{$value->autor}}</em> | Publicado em <em>{{$value->data}}</em></p>
-    </header>
-    <br>
-    <main>
-        <p><big><strong>{{$value->preview}}</strong></big></p>
-        <br>
-        <p><big>{{$value->descricao}}</big></p>
-    </main>
-    <footer>
-        <div class="class3">
-        <a href="/blog" role="button" class="btn btn-default">Voltar á Página Inicial</a>
-        </div>
-    </footer>
-    @endforeach
+    <div class="all">
+        @foreach($previewblog as $value )
+            <img class="class2" src="{{ url('assets/img/paginas/frontend/blog/' .$value->img)}}" ></img>
+                <div class="text">
+                    <h1>{{$value->titulo}}</h1>
+                    <p>Publicado por <em>{{$value->autor}}</em> | Publicado em <em>{{$value->data}}</em></p>
+                    <p><big><strong>{{$value->preview}}</strong></big></p>
+                    <br>
+                    <p><big>{{$value->descricao}}</big></p>
+                </div>
+                <div class="bnt">
+                    <a href="/blog" role="button" class="btn btn-default">Voltar á Página Inicial</a>
+                </div>
+        @endforeach
     </div>
     </body>
         @include('includes.frontend.footer')
