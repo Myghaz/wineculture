8a57a973a0763b1a998a2e236f565c38f2a1cf2f
josecparreira <73337511+josecparreira@users.noreply.github.com>
2020-12-18 11:32:45 +0000

    Update faq.blade.php

--- resources/views/paginas/backend/faq.blade.php
+++ resources/views/paginas/backend/faq.blade.php
@@ -35,45 +35,45 @@
                         <div class="card-body cartao">
                             <h2 class="mb-1">NAda</h2>
                             <p>Produtos Totais (2020)</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <!-- Recent Order Table -->
+                    <div class="card card-table-border-none" id="users">
+                        <div class="card-header justify-content-between">
+                            <h2>Perguntas Frequentes</h2>
+                        </div>
+                        <div class="card-body pt-0 pb-5">
+                            <table id="tablefaq" class="ui celled table" style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th class="d-none d-lg-table-cell">Pergunta</th>
+                                        <th class="d-none d-lg-table-cell">Resposta</th>
+                                        <th class="d-none d-lg-table-cell">Categoria</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        @foreach ($perguntas as $pergunta)
+                                            <td>{{ $pergunta->id }}</td>
+                                            <td>
+                                                <a class="text-dark" href="">{{ $pergunta->pergunta }}</a>
+                                            </td>
+                                            <td>
+                                                <a class="text-dark" href="">{!! $pergunta->resposta !!}</a>
+                                            </td>
+                                            <td class="d-none d-lg-table-cell">{{ $pergunta->categoria }}</td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-12">
-                <!-- Recent Order Table -->
-                <div class="card card-table-border-none" id="users">
-                    <div class="card-header justify-content-between">
-                        <h2>Perguntas Frequentes</h2>
-                    </div>
-                    <div class="card-body pt-0 pb-5">
-                        <table id="tablefaq" class="ui celled table" style="width:100%">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th class="d-none d-lg-table-cell">Pergunta</th>
-                                    <th class="d-none d-lg-table-cell">Resposta</th>
-                                    <th class="d-none d-lg-table-cell">Categoria</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    @foreach ($perguntas as $pergunta)
-                                        <td>{{ $pergunta->id }}</td>
-                                        <td>
-                                            <a class="text-dark" href="">{{ $pergunta->pergunta }}</a>
-                                        </td>
-                                        <td>
-                                            <a class="text-dark" href="">{!! $pergunta->resposta !!}</a>
-                                        </td>
-                                        <td class="d-none d-lg-table-cell">{{ $pergunta->categoria }}</td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@endsection
+    @endsection
