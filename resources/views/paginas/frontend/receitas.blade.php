82be681daae7d1e0b9d320aa74454b8a8a015368
Silvestre0 <73337387+Silvestre0@users.noreply.github.com>
2021-01-06 18:09:08 +0000

    update

---
+++ resources/views/paginas/frontend/receitas.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+
+<head>
+    @section('title', 'Receitas')
+    @include('includes.frontend.head')
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/paginas/frontend/receitas.css') }}">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+
+<body>
+    @include('includes.frontend.navbar')
+    <section>
+        <div class="container">
+
+        
+        </div>
+    </section>
+    <br>
+
+</body>
+@include('includes.frontend.footer')
+</html>
