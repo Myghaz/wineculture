97ec4a56336ea764b799a297a056e8cddc00e684
Silvestre0 <73337387+Silvestre0@users.noreply.github.com>
2021-01-09 19:18:31 +0000

    update edit

--- resources/views/paginas/backend/receitas/edit.blade.php
+++ resources/views/paginas/backend/receitas/edit.blade.php
@@ -13,32 +13,29 @@
       </div>
       @endif
       <div class="form-group">
-        <label for="formGroupExampleInput">Nome da Receita</label>
-        <input type="text" class="form-control" name="titulo" id="formGroupExampleInput" placeholder="Nome da Receita" value="{{$receita->nome}}">
+        <label for="exampleFormControlTextarea1">Nome da Receita</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="nome">{{$receita->nome}}</textarea>
       </div>
 
       <div class="form-group">
         <label for="exampleFormControlSelect1">Categoria</label>
-        <select class="form-control" id="exampleFormControlSelect1"  name="id_categoria">
+        <select class="form-control" id="exampleFormControlSelect1"  name="categoria">
             <option value="">Selecione uma Categoria</option>
             @foreach ($category_wines as $category_wine)
             <option value="{{$category_wine->id}}"> {{$category_wine->nome}}</option>
             @endforeach
         </select>
     </div>
-
       <div class="form-group row">
         <label for="staticEmail" class="col-sm-2 col-form-label">Data</label>
         <div class="col-sm-10">
           <input type="text" readonly class="form-control-plaintext" name="data" id="staticEmail" value="{{now()}}">
         </div>
       </div>
-
       <div class="form-group">
         <label for="exampleFormControlTextarea1">Descrição</label>
         <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="descricao">{{$receita->descricao}}</textarea>
       </div>
-
       <div class="form-group">
         <label for="exampleFormControlFile1">Submeter Imagem</label>
         <input type="file" class="form-control-file" id="exampleFormControlFile1" name="img">
