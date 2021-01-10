8909c37d5630913741dfd34f12266f37abacd69d
Silvestre0 <73337387+Silvestre0@users.noreply.github.com>
2021-01-10 18:22:29 +0000

    update

--- resources/views/paginas/frontend/receitas.blade.php
+++ resources/views/paginas/frontend/receitas.blade.php
@@ -8,13 +8,51 @@
     <link rel="stylesheet" href="{{ URL::asset('assets/css/paginas/frontend/bootstrap.css') }}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
 </head>
 
 <body>
     @include('includes.frontend.navbar')
     <section>
         <img src="assets\img\paginas\frontend\receitas\banner-vinho1.jpg" alt="">
-<div class="container">
+      <div class="filtro">
+        <article class="card-group-item">
+            <div class="filter-content">
+                <div class="card-body">
+                    <div class="custom-control custom-checkbox">
+                        <span class="float-right badge badge-light round">2</span>
+                          <input type="checkbox" class="custom-control-input" id="Check1">
+                          <label class="custom-control-label" for="Check1">Vinho Tinto</label>
+                    </div> <!-- form-check.// -->
+
+                    <div class="custom-control custom-checkbox">
+                        <span class="float-right badge badge-light round">2</span>
+                          <input type="checkbox" class="custom-control-input" id="Check2">
+                         <label class="custom-control-label" for="Check2">Vinho Branco</label>
+                    </div> <!-- form-check.// -->
+
+                    <div class="custom-control custom-checkbox">
+                        <span class="float-right badge badge-light round">7</span>
+                          <input type="checkbox" class="custom-control-input" id="Check3">
+                          <label class="custom-control-label" for="Check3">Vinho Verde</label>
+                    </div> <!-- form-check.// -->
+
+                    <div class="custom-control custom-checkbox">
+                        <span class="float-right badge badge-light round">7</span>
+                          <input type="checkbox" class="custom-control-input" id="Check4">
+                          <label class="custom-control-label" for="Check4">Vinho Rosé</label>
+                    </div> <!-- form-check.// -->
+
+                    <div class="custom-control custom-checkbox">
+                        <span class="float-right badge badge-light round">7</span>
+                          <input type="checkbox" class="custom-control-input" id="Check5">
+                          <label class="custom-control-label" for="Check5">Espumante</label>
+                    </div> <!-- form-check.// -->
+                </div> <!-- card-body.// -->
+            </div>
+        </article> <!-- card-group-item.// -->
+    </div>
+        <div class="container">
 <br>
     <div class="row">
         @foreach($receitas as $value)
