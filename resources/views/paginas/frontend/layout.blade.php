b6cc7e863eea7c0f255c59152f918ceb7ce44ab3
Vintage <miguel.ciencia655@gmail.com>
2021-01-11 20:54:39 +0000

    sadasd

--- resources/views/paginas/frontend/layout.blade.php
+++ resources/views/paginas/frontend/layout.blade.php
@@ -25,7 +25,6 @@
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-  <link rel="stylesheet" href="{{ URL::asset('assets/css/paginas/frontend/bootstrap.css') }}">
 
   <link rel="stylesheet" href="{{ URL::asset('assets/css/paginas/frontend/receitas.css') }}">
 
