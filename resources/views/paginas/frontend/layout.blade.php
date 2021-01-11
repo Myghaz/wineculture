f14e5c94867150e0dfa83fe36b404f45e74992bc
Silvestre0 <73337387+Silvestre0@users.noreply.github.com>
2021-01-11 18:08:29 +0000

    layout

--- resources/views/paginas/frontend/layout.blade.php
+++ resources/views/paginas/frontend/layout.blade.php
@@ -18,11 +18,12 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js"></script>
-
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/includes/frontend/navbar.css') }}">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
     <link rel="stylesheet" href="{{ URL::asset('assets/css/paginas/frontend/bootstrap.css') }}">
 
-    <link rel="stylesheet" href="{{ URL::asset('assets/css/includes/frontend/navbar.css') }}">
+
     <link rel="stylesheet" href="{{ URL::asset('assets/css/includes/frontend/settings.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('assets/css/paginas/frontend/index.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('assets/css/paginas/frontend/receitas.css') }}">
