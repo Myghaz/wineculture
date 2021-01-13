5f3daa7d691749369b4b6a768de93b81efb5f57c
Vintage <miguel.ciencia655@gmail.com>
2021-01-13 06:01:49 +0000

    UPDATE GERAL CSS E JS SEPARADOS

--- resources/views/paginas/frontend/previewblog.blade.php
+++ resources/views/paginas/frontend/previewblog.blade.php
@@ -1,31 +1,28 @@
-<!doctype html>
-<html>
+@extends("paginas.frontend.layout")
 
-<head>
-    @section('title', 'Ler Mais')
-    @include('includes.frontend.head')
-    <link rel="stylesheet" href="{{ URL::asset('assets/css/paginas/frontend/previewblog.css') }}">
-    @include('includes.frontend.navbar')
-</head>
+@section('title', $previewblog->titulo)
 
-<body>
-    <div class="all">
-            <img class="class2" src="{{ url('assets/img/paginas/frontend/blog/' .$previewblog->img)}}" ></img>
-                <div class="text">
-                    <h1>{{$previewblog->titulo}}</h1>
-                    <p>Publicado por <em>{{$previewblog->autor}}</em> | Publicado em <em>{{$previewblog->data}}</em></p>
-                    <p><big><strong>{{$previewblog->preview}}</strong></big></p>
-                    {!!$previewblog->descricao!!}
-                </div>
-                <div class="bnt">
-                    <a href="/blog" role="button" class="btn btn-default">Voltar á Página Inicial</a>
+@section("links")
+<link rel="stylesheet" href="{{ URL::asset('assets/css/paginas/frontend/previewblog.css') }}">
+@endsection
 
-                     </div>
+@section("content")
+
+<div class="all">
+    <img class="class2" src="{{ url('assets/img/paginas/frontend/blog/' .$previewblog->img)}}"></img>
+    <div class="text">
+        <h1>{{$previewblog->titulo}}</h1>
+        <p>Publicado por <em>{{$previewblog->autor}}</em> | Publicado em <em>{{$previewblog->data}}</em></p>
+        <p><big><strong>{{$previewblog->preview}}</strong></big></p>
+        {!!$previewblog->descricao!!}
     </div>
-    <br>
-    <br>
-    <br>
+    <div class="bnt">
+        <a href="/blog" role="button" class="btn btn-default">Voltar á Página Inicial</a>
 
-    </body>
-        @include('includes.frontend.footer')
-</html>
+    </div>
+</div>
+<br>
+<br>
+<br>
+
+@endsection
