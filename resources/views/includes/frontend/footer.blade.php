cd07cae2a2316a9cd649da224986a7043a246f8c
Meireles09 <guimeireles90@gmail.com>
2021-01-15 10:23:52 +0000

    Alter NavBar & New Footer
    Adicionado dropdown á navbar
    Alterado por completo o Footer

--- resources/views/includes/frontend/footer.blade.php
+++ resources/views/includes/frontend/footer.blade.php
@@ -1,37 +1,45 @@
 <link rel="stylesheet" href="{{ URL::asset('assets/css/includes/frontend/footer.css') }}">
 
-<footer class="footer-distributed">
+<footer>
+    <div id="container">
+        <div id="part1">
+            <div id="companyinfo"> <a id="sitelink" href="/">WineCulture</a>
+                <p id="title">Cool and Perfect Snippet code</p>
+                <p id="detail">We create awesome code snippets that will help you in creating your own beautiful site. </p>
+            </div>
+            <div id="explore">
+                <p id="txt1">Explore</p>
+                <a class="link" href="/">Página Principal</a>
+                  <a class="link" href="/sobre">Sobre Nós</a>
+                   <a class="link" href="/receitas">Receitas</a>
+                   <a class="link" href="/blog">Notícias</a>
+            </div>
+            <div id="visit">
+                <p id="txt2">Visit</p>
+                <p class="text" href="/vinhos">Vinho</p>
+                <p class="text" href="/sobre_vinhos">Sobre Vinhos</p>
+                <p class="text" href="#">Loja</p>
+                <p class="text" href="/faq">FaQ</p>
+                <p class="text" href="/contactos">Contactos</p>
 
-    <div class="footer-right">
-
-        <a href="https://www.facebook.com/WineCulture.pt/"><i class="fa fa-facebook"></i></a>
-        <a href="https://www.instagram.com/wineculture.geral/"><i class="fa fa-instagram"></i></a>
-        <a href="#"><i class="fa fa-linkedin"></i></a>
-        <a href="#"><i class="fa fa-github"></i></a>
-
+            </div>
+            <div id="legal">
+                <p id="txt3">Legal</p>
+                 <a class="link1" href="#">Termos e Condições</a>
+                 <a class="link1" href="/politica_de_privacidade">Politica de Privacidade</a>
+            </div>
+            <div id="subscribe">
+                <p id="txt4">Subscribe to US</p>
+                <form> <input id="email" type="email" placeholder="Email"> </form> <a class="waves-effect waves-light btn">Subscribe</a>
+                <p id="txt5">Connect With US</p>
+                <a href="https://www.facebook.com/WineCulture.pt/"><i class="fa fa-facebook"></i></a>
+                <a href="https://www.instagram.com/wineculture.geral/"><i class="fa fa-instagram"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-github"></i></a>
+            </div>
+        </div>
+        <div id="part2">
+            <p id="txt6"><i > WineCulture &copy; 2021 - All right reserved</p>
+        </div>
     </div>
-
-    <div class="footer-left">
-
-        <p class="footer-links">
-            <a class="link-1" href="/">Página Principal</a>
-
-            <a href="/blog">Blog</a>
-
-            <a href="/sobre">Sobre Nós</a>
-
-            <a href="/receitas">Receitas</a>
-
-            <a href="#">Loja</a>
-
-            <a href="/faq">FaQ</a>
-
-            <a href="/contactos">Contactos</a>
-
-            <a href="/politica_de_privacidade">Politica de Privacidade</a>
-        </p>
-
-        <p>WineCulture &copy; 2021</p>
-    </div>
-
 </footer>
--- resources/views/includes/frontend/navbar.blade.php
+++ resources/views/includes/frontend/navbar.blade.php
@@ -3,10 +3,22 @@
         <div class="img" id="imgnav"> </div>
         <ul>
             <li @if (Route::currentRouteName()=='index' ) class="active" @endif><a href="/">Início</a></li>
-            <li @if (Route::currentRouteName()=='vinhos' ) class="active" @endif><a href="/vinhos">Vinhos</a></li>
-            <li @if (Route::currentRouteName()=='sobre_vinhos' ) class="active" @endif><a href="/sobre_vinhos">Sobre Vinhos</a></li>
-            <li @if (Route::currentRouteName()=='receitas' ) class="active" @endif><a href="/receitas">Receitas</a></li>
             <li @if (Route::currentRouteName()=='sobre' ) class="active" @endif><a href="/sobre">Sobre</a></li>
+            <li>
+                <div class="dropdown">
+                    <button class="dropbtn">Vinho</button>
+                    <div class="dropdown-content">
+                        <a href="/vinhos">Vinhos</a>
+                        <a href="/sobre_vinhos">Sobre Vinhos</a>
+                    </div>
+                  </div>
+                 </li>
+            <!--<li @if (Route::currentRouteName()=='vinhos' ) class="active" @endif><a href="/vinhos">Vinhos</a></li>
+            <li @if (Route::currentRouteName()=='sobre_vinhos' ) class="active" @endif><a href="/sobre_vinhos">Sobre Vinhos</a></li>-->
+            <li @if (Route::currentRouteName()=='blog' ) class="active" @endif><a href="/blog">Notícias</a></li>
+            <li @if (Route::currentRouteName()=='receitas' ) class="active" @endif><a href="/receitas">Receitas</a></li>
+
+
             <li @if (Route::currentRouteName()=='contactos' ) class="active" @endif><a href="/contactos">Contactos</a></li>
             <li><a target="_blank" href="http://wineculture.test/loja/">Loja</a></li>
             @if (!Auth::check()) <li @if (Route::currentRouteName()=='login' ) class="active" @endif><a href="{{route('login')}}">Login</a></li>@elseif (Auth::user()->tipouser == "Administrador") <li><a href="{{route('admin_dashboard')}}">Admin</a></li>@endif
