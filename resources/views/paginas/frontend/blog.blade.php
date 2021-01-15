75e7696405b06628f728ff95a7ef50dd06f5f4ff
Vintage <miguel.ciencia655@gmail.com>
2021-01-15 01:15:22 +0000

    UPDATE BLOG

--- resources/views/paginas/frontend/blog.blade.php
+++ resources/views/paginas/frontend/blog.blade.php
@@ -9,23 +9,43 @@
 @section("content")
 
 <section>
-    <div class="container">
-        <main>
-            @foreach($blogs as $value)
-            <div class="singleBlog">
-                <img src="{{ url('assets/img/paginas/frontend/blog/' .$value->img)}}" alt="">
-                <div class="blogContent">
-                    <h3>{{$value->titulo}}<span>{{$value->categoria}}</span></h3>
-                    <p>By <a class="a">{{$value->autor}}</a> | <a class="a">{{$value->data}}</a></p>
-                    <a href="{{ route('previewblog', $value) }}" class="btn">Ler Mais</a>
+<div class="containerr">
+    <div class="card-column column-0">
+    @foreach($blogs as $value)
+        <div class="card card-color-0" id="{{$value->id}}">
+        <input type="hidden" name="id" value="{{$value->id}}">
+            <div class="border"></div>
+            <img src="{{ url('assets/img/paginas/frontend/blog/' .$value->img)}}"/>
+            <div style="display: none;" id="text{{$value->id}}">
+           
+                <h1 id="titulo">{{$value->titulo}}</h1>
+                <p id="data">{{$value->created_at}}</p>
+                <p id="desc">{{$value->preview}}</p>
+                <div class="autor" id="autor">
+                    <img src="http://wineculture.test/wp-content/uploads/2020/10/banner5.jpg" /><span class="nome">Nome</span><span>Apelido</span>
                 </div>
             </div>
-            @endforeach
-        </main>
+            <div class="texto">
+                <h2 id="texto_titulo" class="info">{{$value->titulo}}<span class="categoria">{{$value->id_categoria}}</span></h2>
+                <a class="info">Ler Mais</a>
+            </div>
+        </div>
+        @endforeach
     </div>
+</div>
+
+
+<div id="cover" class="cover"></div>
+
+<div id="open-content" class="open-content">
+    <a href="#" id="close-content" class="close-content"><span class="x-1"></span><span class="x-2"></span></a>
+    <img id="open-content-image" src="">
+    <div class="text" id="open-content-text">
+    </div>
+</div>
+</div>
 </section>
 @section("javascript")
-<script src="{{ URL::asset('assets/css/paginas/frontend/vendor/aos/aos.js') }}"></script>
-<script src="{{ URL::asset('assets/js/paginas/frontend/sobre_vinhos.js') }}"></script>
+<script src="{{ URL::asset('assets/js/paginas/frontend/blog.js') }}"></script>
 @endsection
 @endsection
