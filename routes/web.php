df1a3d41f6c6eb97dd6c8bf6719f14690d709b90
Silvestre0 <73337387+Silvestre0@users.noreply.github.com>
2021-01-20 18:14:21 +0000

    web.php

--- routes/web.php
+++ routes/web.php
@@ -42,13 +42,7 @@ Route::get('/receitas', [ReceitasController::class, 'indexFrontend'])->name('rec
 
 
 //MensagensController
-<<<<<<< Updated upstream
 Route::get('/contactos', [MensagensController::class, 'indexFrontend'])->name('contactos');
-=======
-Route::get('/contactos', [MensagensController::class, 'indexFrontend'])->name('contactos_frontend_store');
-
-Route::post('/contactos', [MensagensController::class, 'frontend_store'])->name('index_contactos_frontend');
->>>>>>> Stashed changes
 
 Route::post('/contactos', [MensagensController::class, 'frontend_store'])->name('contactos_frontend_store');
 
