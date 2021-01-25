c525263fbc893469d0b774bf1e93b77fa96dd8b6
Vintage <miguel.ciencia655@gmail.com>
2021-01-25 20:05:06 +0000

    footer

--- resources/views/paginas/frontend/layout.blade.php
+++ resources/views/paginas/frontend/layout.blade.php
@@ -4,7 +4,6 @@
 <head>
   <title>WineCulture - @yield('title')</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js'></script>
   <link rel="stylesheet" href="{{ URL::asset('assets/css/paginas/frontend/vendor/icofont/icofont.min.css') }}">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
   <link rel="stylesheet" href="{{ URL::asset('assets/css/includes/frontend/footer.css') }}">
@@ -29,18 +28,16 @@
     background-position: center;
     background-repeat: no-repeat;
 	background-size: cover; padding-bottom:130px">
+
     @yield("content")
-
-    <a href="# " class="back-to-top "><i class="icofont-simple-up "></i></a>
-
-    <script src="{{ URL::asset('assets/js/layouts/frontend/navbar.js') }}"></script>
-    <script src="{{ URL::asset('assets/js/layouts/frontend/settings.js') }}"></script>
   </div>
-
+  <footer>
+    @include('includes.frontend.footer')
+  </footer>
+  <a href="# " class="back-to-top "><i class="icofont-simple-up "></i></a>
+  <script src="{{ URL::asset('assets/js/layouts/frontend/navbar.js') }}"></script>
   @yield("javascript")
-@include('includes.frontend.footer')
 </body>
-
 <script>
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
