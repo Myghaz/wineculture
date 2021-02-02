a5165b44f37bf2acd98a7c97169632fce925f16a
josecparreira <73337511+josecparreira@users.noreply.github.com>
2021-02-02 01:32:26 +0000

    email teste

--- resources/views/paginas/frontend/contactosresposta.blade.php
+++ resources/views/paginas/frontend/contactosresposta.blade.php
@@ -5,18 +5,18 @@
 <head>
     <title>Wineculture</title>
     <style>
-   
+
 </style>
 </head>
 <body>
     <p>Caro {{$nome}},</p>
     <p>A resposta à sua pergunta:</p>
     <p>"{{$pergunta}}"</p>
-    
+
     <p>É a seguinte:</p>
-    <p>"{{$resposta}}"</p>
-   
-    <p>Com os melhores cumprimentos, 
+    <p>"{!!$resposta!!}"</p>
+
+    <p>Com os melhores cumprimentos,
     <br>A Equipa do WineCutlure</p>
 </body>
 </html>
