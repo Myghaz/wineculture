2c432eea179783bb21a5cc37b4ff6524c17bc75b
Vintage <miguel.ciencia655@gmail.com>
2021-02-05 06:51:14 +0000

    update login

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -26,18 +26,6 @@ class MainController extends Controller
     {
         return view('paginas.frontend.sobre_vinhos');
     }
-
-    /*public function faq() {
-        $perguntas_conta = Perguntas::where('categoria', '=', "Conta")->get();
-        $perguntas_encomenda = Perguntas::where('categoria', '=', "Encomenda")->get();
-        $perguntas_pagamento = Perguntas::where('categoria', '=', "Pagamento")->get();
-        $perguntas_teste = Perguntas::where('categoria', '=', "teste")->get();
-        $categorias = CategoriaPergunta::all('categoria')->select('categoria')->distinct()->orderBy('categoria')->get();
-        //$pergunta_sconta= Perguntas::where('categoria', '=', "Conta")-get();
-        return view('paginas.frontend.FAQ', ['perguntas_conta' => $perguntas_conta, 'perguntas_teste' => $perguntas_teste, 'perguntas_encomenda' => $perguntas_encomenda,
-         'perguntas_pagamento' => $perguntas_pagamento, 'categorias' => $categorias]);
-    }*/
-
     public function pdp()
     {
         return view('paginas.frontend.pdp');
