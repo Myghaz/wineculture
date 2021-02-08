d121d3c3ede971bef3251d1ab30f62e732aeb6ae
PedrocasMonteiro <PedrocasMonteiro@users.noreply.github.com>
2021-02-08 17:29:06 +0000

    update 2

--- resources/views/paginas/frontend/sobre.blade.php
+++ resources/views/paginas/frontend/sobre.blade.php
@@ -34,12 +34,11 @@
       <div class="col-md-5 mt-5">
         <h2 class="sobre_color mb-5"><b>Missão</b></h2>
         <p>
-          A WineCulture é uma instituição sem fins lucrativos, com o objetivo de promover a vinicultura nacional.<br><br>
-          Com o nosso website nós listamos todos os nossos vinhos, com toda a sua informação e com o seu acompanhamento perfeito.<br><br>
+          A missão da nossa instituição é promover a vinicultura nacional.<br><br>
+          Com o nosso website é possivel descobrir novos vinhos de maneira rápida, sendo possível filtrar a sua pesquisa por categoria, o seu produtor, a sua classificação ou a sua região!<br><br>
 
-          A nossa instituição é excelente para os produtores de vinho de Portugal, se for um produtor Português pode contactar-nos para publicar os seus vinhos, promovendo assim a vinicultura nacional.<br><br>
-          Temos também uma parte comercial onde vendemos mercadoria como acessórios pessoais, de escritório, vestuário e produtos para se proteger durante a pandemia.<br><br>
-          Conte conosco para descobrir a vinicultura nacional!
+          Para além de pesquisar vinhos é também possível encontrar os acompanhamentos mais adequados para o seu vinho de escolha.<br><br>
+
         </p>
       </div>
     </div>
@@ -52,198 +51,124 @@
   </div>
 
   <div class="mt-5 ml-3 mr-3 pb-4 pl-3 pr-3" data-aos="fade-up" data-aos-delay="50">
-    <h2 class="sobre_color"><b>Nossa Equipa</b></h2>
+    <h2 class="sobre_color text-center"><b>Nossa Equipa</b></h2>
   </div>
 
-  <section>
-  
-    <div class="cont s--inactive mb-5" data-aos="fade-up" data-aos-delay="150">
-  <!-- cont inner start -->
-  <div class="cont__inner">
-    <!-- el start -->
-    <div class="el">
-      <div class="el__overflow">
-        <div class="el__inner">
-          <div class="el__bg"></div>
-          <div class="el__preview-cont">
-            <h2 class="el__heading">Sérgio Silvestre</h2>
-          </div>
-          <div class="el__content">
-            <div class="el__text">Full-Stack Developer</div>
-            <div class="el__close-btn"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- el end -->
-    <!-- el start -->
-    <div class="el">
-      <div class="el__overflow">
-        <div class="el__inner">
-          <div class="el__bg"></div>
-          <div class="el__preview-cont">
-            <h2 class="el__heading">Guilherme Meireles</h2>
-          </div>
-          <div class="el__content">
-            <div class="el__text">Front-End Developer</div>
-            <div class="el__close-btn"></div>
+  <div class="container mb-5" data-aos="fade-up" data-aos-delay="80">
+
+  <div class="row team-row">
+
+    <div class="col-md-3 col-sm-6 team-wrap">
+      <div class="team-member text-center">
+        <div class="team-img">
+          <img src="/assets/img/paginas/frontend/index/team/team-1.jpg" alt="Sérgio Silvestre">
+          <div class="overlay">
+            <div class="team-details text-center">
+              
+              <div class="socials mt-5">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-google-plus"></i></a>
+                <a href="#"><i class="fa fa-envelope"></i></a>
+              </div>
+            </div>
           </div>
         </div>
+        <h6 class="team-title">Sérgio Silvestre</h6>
+        <span>Full-Stack Developer</span>
       </div>
     </div>
-    <!-- el end -->
-    <!-- el start -->
-    <div class="el">
-      <div class="el__overflow">
-        <div class="el__inner">
-          <div class="el__bg"></div>
-          <div class="el__preview-cont">
-            <h2 class="el__heading">José Parreira</h2>
-          </div>
-          <div class="el__content">
-            <div class="el__text">Full-Stack Developer</div>
-            <div class="el__close-btn"></div>
+    <!-- end team member -->
+
+    <div class="col-md-3 col-sm-6 team-wrap">
+      <div class="team-member text-center">
+        <div class="team-img">
+          <img src="/assets/img/paginas/frontend/index/team/team-2.jpg" alt="Guilherme Meireles">
+          <div class="overlay">
+            <div class="team-details text-center">
+
+              <div class="socials mt-5">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-google-plus"></i></a>
+                <a href="#"><i class="fa fa-envelope"></i></a>
+              </div>
+            </div>
           </div>
         </div>
+        <h6 class="team-title">Guilherme Meireles</h6>
+        <span>Front-End Developer</span>
       </div>
     </div>
-    <!-- el end -->
-    <!-- el start -->
-    <div class="el">
-      <div class="el__overflow">
-        <div class="el__inner">
-          <div class="el__bg"></div>
-          <div class="el__preview-cont">
-            <h2 class="el__heading">Miguel Pereira</h2>
-          </div>
-          <div class="el__content">
-            <div class="el__text">Back-end Developer</div>
-            <div class="el__close-btn"></div>
+    <!-- end team member -->
+    
+    <div class="col-md-3 col-sm-6 team-wrap">
+      <div class="team-member last text-center">
+        <div class="team-img">
+          <img src="/assets/img/paginas/frontend/index/team/team-3.jpg" alt="José Parreira">
+          <div class="overlay">
+            <div class="team-details text-center">
+              
+              <div class="socials mt-5">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-google-plus"></i></a>
+                <a href="#"><i class="fa fa-envelope"></i></a>
+              </div>
+            </div>
           </div>
         </div>
+        <h6 class="team-title">José Parreira</h6>
+        <span>Full-Stack Developer</span>
       </div>
     </div>
-    <!-- el end -->
-    <!-- el start -->
-    <div class="el">
-      <div class="el__overflow">
-        <div class="el__inner">
-          <div class="el__bg"></div>
-          <div class="el__preview-cont">
-            <h2 class="el__heading">Pedro Monteiro</h2>
-          </div>
-          <div class="el__content">
-            <div class="el__text">Designer</div>
-            <div class="el__close-btn"></div>
+    
+    <div class="col-md-3 col-sm-6 team-wrap">
+      <div class="team-member last text-center">
+        <div class="team-img">
+          <img src="/assets/img/paginas/frontend/index/team/team-4.jpg" alt="Miguel Pereira">
+          <div class="overlay">
+            <div class="team-details text-center">
+              
+              <div class="socials mt-5">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-google-plus"></i></a>
+                <a href="#"><i class="fa fa-envelope"></i></a>
+              </div>
+            </div>
           </div>
         </div>
+        <h6 class="team-title">Miguel Pereira</h6>
+        <span>Back-End Developer</span>
       </div>
     </div>
-    <!-- el end -->
+    <div class="col-md-3 col-sm-6 team-wrap">
+      <div class="team-member last text-center">
+        <div class="team-img">
+          <img src="/assets/img/paginas/frontend/index/team/team-5.jpg" alt="Pedro Monteiro">
+          <div class="overlay">
+            <div class="team-details text-center">
+              
+              <div class="socials mt-5">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-google-plus"></i></a>
+                <a href="#"><i class="fa fa-envelope"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h6 class="team-title">Pedro Monteiro</h6>
+        <span>Designer</span>
+      </div>
+    </div>
+    <!-- end team member -->
+
   </div>
-  <!-- cont inner end -->
 </div>
 
-    </section>
 </div>
-
-<!-- ======= Our Team Section ======= -->
-<!--<section id="team" class="team">
-        <div class="teamcontainer">
-
-            <div class="section-title">
-                <h2>Nossa Equipa</h2>
-                <p>Somos uma equipa de 5 jovens muito empenhados a trabalhar neste projeto. Venha nos conhecer.</p>
-            </div>
-
-            <div class="row">
-
-                <div data-aos="fade-up">
-                    <div class="member">
-                        <div class="pic"><img src="/assets/img/paginas/frontend/index/team/team-1.jpg" width="300" height="300" alt=""></div>
-                        <div class="member-info">
-                            <h4>Sérgio Silvestre</h4>
-                            <span>Full-Stack Developer</span>
-                            <div class="social">
-                                <a href=""><i class="icofont-twitter"></i></a>
-                                <a href=""><i class="icofont-facebook"></i></a>
-                                <a href=""><i class="icofont-instagram"></i></a>
-                                <a href=""><i class="icofont-linkedin"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div data-aos="fade-up" data-aos-delay="100">
-                    <div class="member">
-                        <div class="pic "><img src="/assets/img/paginas/frontend/index/team/team-2.jpg " width="300" height="300" alt=" "></div>
-                        <div class="member-info ">
-                            <h4>Guilherme Meireles</h4>
-                            <span>Front-end Developer</span>
-                            <div class="social ">
-                                <a href=" "><i class="icofont-twitter "></i></a>
-                                <a href=" "><i class="icofont-facebook "></i></a>
-                                <a href=" "><i class="icofont-instagram "></i></a>
-                                <a href=" "><i class="icofont-linkedin "></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div data-aos="fade-up" data-aos-delay="200">
-                    <div class="member">
-                        <div class="pic ">
-                            <img src="/assets/img/paginas/frontend/index/team/team-3.jpg " width="300" height="300" alt=" ">
-                        </div>
-                        <div class="member-info ">
-                            <h4>José Parreira</h4>
-                            <span>Full-Stack Developer</span>
-                            <div class="social ">
-                                <a href=" "><i class="icofont-twitter "></i></a>
-                                <a href=" "><i class="icofont-facebook "></i></a>
-                                <a href=" "><i class="icofont-instagram "></i></a>
-                                <a href=" "><i class="icofont-linkedin "></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div data-aos="fade-up" data-aos-delay="300">
-                    <div class="member">
-                        <div class="pic "><img src="/assets/img/paginas/frontend/index/team/team-4.jpg " width="300" height="300" alt=" "></div>
-                        <div class="member-info ">
-                            <h4>Miguel Pereira</h4>
-                            <span>Back-end Developer</span>
-                            <div class="social ">
-                                <a href=" "><i class="icofont-twitter "></i></a>
-                                <a href=" "><i class="icofont-facebook "></i></a>
-                                <a href=" "><i class="icofont-instagram "></i></a>
-                                <a href=" "><i class="icofont-linkedin "></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div data-aos="fade-up" data-aos-delay="400">
-                    <div class="member">
-                        <div class="pic "><img src="/assets/img/paginas/frontend/index/team/team-5.jpg " width="300" height="300" alt=" "></div>
-                        <div class="member-info ">
-                            <h4>Pedro Monteiro</h4>
-                            <span>Designer</span>
-                            <div class="social ">
-                                <a href=" "><i class="icofont-twitter "></i></a>
-                                <a href=" "><i class="icofont-facebook "></i></a>
-                                <a href=" "><i class="icofont-instagram "></i></a>
-                                <a href=" "><i class="icofont-linkedin "></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-        </div>
-    </section>-->
     
     
 
